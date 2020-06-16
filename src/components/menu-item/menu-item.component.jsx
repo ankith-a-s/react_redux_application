4a6827dca29f659e6adba4b356d1423dab8c9616
Ankith A S <ankith.s@zetwerk.com>
2020-06-16 16:52:57 +0530

    added styling for menu item

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,11 @@ import './menu-item.styles.scss';
 
 const MenuItem = ({ size, title, imageUrl }) => {
   return (
-    <div className={`${size} menu-item`} style={{ backgroundImage: `url(${imageUrl})` }}>
+    <div className={`${size} menu-item`}>
+      <div
+        className='background-image'
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className='content'>
         <h1 className='title'>{title}</h1>
         <span>SHOP NOW</span>
