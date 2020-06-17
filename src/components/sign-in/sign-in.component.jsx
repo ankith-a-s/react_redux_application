940a4049b745bfafda381742aff903f18b16c6df
Ankith A S <ankith.s@zetwerk.com>
2020-06-17 10:14:10 +0530

    design improvements in button and form input

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+export default class SignIn extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  handleChange = (e) => {
+    const { value, name } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            label="email"
+            handleChange={this.handleChange}
+            required
+            name='email'
+            value={email}
+            type='email'
+          />
+          <FormInput
+            label='password'
+            handleChange={this.handleChange}
+            name='password'
+            value={password}
+            type='password'
+            required
+          />
+          <CustomButton
+            type='submit'
+            value='Submit'
+            children='Sign In'
+          />
+        </form>
+      </div>
+    );
+  }
+}
